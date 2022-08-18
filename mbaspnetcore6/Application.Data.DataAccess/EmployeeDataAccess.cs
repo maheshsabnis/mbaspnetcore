3deb79f833b3ef805a009a81d62ec230caedac9a
maheshsabnis <sabnis_m@hotmail.com>
2022-08-18 15:33:27 +0530

    eee

---
+++ mbaspnetcore6/Application.Data.DataAccess/EmployeeDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Application.Dal.Contract;
+using Application.Entities;
+namespace Application.Data.DataAccess;
+
+public class EmployeeDataAccess : IDataAccess<Employee, int>
+{
+    Employee IDataAccess<Employee, int>.Create(Employee entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    Employee IDataAccess<Employee, int>.Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerable<Employee> IDataAccess<Employee, int>.Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    Employee IDataAccess<Employee, int>.Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    Employee IDataAccess<Employee, int>.Update(int id, Employee entity)
+    {
+        throw new NotImplementedException();
+    }
+}
+ 
